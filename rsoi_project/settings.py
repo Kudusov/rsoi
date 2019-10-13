49630b2f031b96fcd60513c6bf8af9b28110b484
Mahmud <qudusov98@mail.ru>
2019-10-13 22:01:21 +0300

    Implement database connection

--- rsoi_project/settings.py
+++ rsoi_project/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'rsoi_project.wsgi.application'
 # }
 
 DATABASES = {} 
-DATABASES ['default'] = dj_database_url.config (conn_max_age = 60)
+DATABASES['default'] = dj_database_url.config(conn_max_age = 600)
 
 
 # Password validation
